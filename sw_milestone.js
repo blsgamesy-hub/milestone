e2679fde34c658c95f92c66b0cb651be15363f70
blsgamesy-hub <bls.game.sy@gmail.com>
2026-07-01 14:52:56 +0900

    Add files via upload

---
+++ sw_milestone.js
@@ -0,0 +1,27 @@
+const CACHE = "milestone-v2";
+const ASSETS = [
+  "./milestone.html",
+  "./manifest_milestone.json",
+  "./icon_192_milestone.png",
+  "./icon_512_milestone.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
